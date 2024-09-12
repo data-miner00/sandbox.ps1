193f845ff592a11fa7ea1c5d68f95873279ce8fd
data-miner00 <data-miner00@users.noreply.github.com>
2024-09-12 22:13:06 +0800

    feat: Add script to remove subdirectories

---
+++ scripts/Remove-Subdirectories.ps1
@@ -0,0 +1,12 @@
+# https://stackoverflow.com/questions/755382/i-want-to-delete-all-bin-and-obj-folders-to-force-all-projects-to-rebuild-everyt
+Param (
+    [Parameter(Mandatory=$true)]
+    [String[]]$Directories
+)
+
+Get-ChildItem .\ -Include $Directories -Recurse | foreach ($_) {
+    Write-Host "Removing $($_.fullname)"
+    Remove-Item $_.fullname -Force -Recurse
+}
+
+Write-Host "Removed contents from '$([string]::Join(', ', $Directories))' subdirectories."
